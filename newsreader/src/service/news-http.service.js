d747272673f7f28d8cbf5b028c679cc888765a0a
ChinmayIngale <chinmay274@gmail.com>
2023-08-12 15:51:38 +0000

    database creation

--- newsreader/src/service/news-http.service.js
+++ newsreader/src/service/news-http.service.js
@@ -1,5 +1,17 @@
 import http from "../http-common";
 
 export const getAllCategories = () => {
-    return http.get("api/news/countries");
+    return http.get("/api/news/countries");
 };
+
+export const getHeadlinesByCategory = (categoryIds) => {
+    return http.get(`/api/news/headlines?categories=${categoryIds}`);
+}
+
+export const getArticleById = (articleId) => {
+    return http.get(`/api/news/article/${articleId}`);
+}
+
+export const postLikeOnArticle = (articleId) => {
+    return http.post(`/api/news/article/${articleId}/like`);
+}
