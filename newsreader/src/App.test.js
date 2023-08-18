de14d02c40030181a650a4bc93d6283f26af183d
ChinmayIngale <chinmay274@gmail.com>
2023-08-18 23:37:26 +0000

    unite testing

--- newsreader/src/App.test.js
+++ newsreader/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Navbar from './components/navbar.component';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('navbar test', () => {
+  render(<Navbar />);
+  const linkElement = screen.getByText(/News Reader/i);
   expect(linkElement).toBeInTheDocument();
 });
